6f85e5efb85bbd32321fd3264658ae99f0f9b228
Raviteja582 <raviteja1028312@gmail.com>
2024-11-24 12:53:04 -0500

    Added Content for Image selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+const Color primaryColor = Color(0xFF76736A);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'hiFriends',
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Initialize Firebase
+  runApp(const ImageIdentificationApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ImageIdentificationApp extends StatelessWidget {
+  const ImageIdentificationApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Messaging',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ImageRecognitionScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ImageRecognitionScreen extends StatefulWidget {
+  const ImageRecognitionScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _LabelImageScreenState createState() => _LabelImageScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LabelImageScreenState extends State<ImageRecognitionScreen> {
+  File? _pickedImage;
+  List<String> _labels = [];
+  final ImagePicker _picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Function to pick an image from the camera or gallery
+  Future<void> _selectImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _pickedImage = File(pickedFile.path);
+      });
+      _labelImage();
+    }
+  }
+
+  // Function to label objects in the selected image using Firebase ML Kit
+  Future<void> _labelImage() async {
+    if (_pickedImage == null) return;
+
+    final inputImage = InputImage.fromFile(_pickedImage!);
+
+    final imageLabeler = ImageLabeler(
+      options:
+          ImageLabelerOptions(confidenceThreshold: 0.5), // Confidence threshold
+    );
+
+    try {
+      final List<ImageLabel> labels =
+          await imageLabeler.processImage(inputImage);
+
+      setState(() {
+        _labels = labels
+            .map((label) =>
+                '${label.label} (${(label.confidence * 100).toStringAsFixed(2)}%)')
+            .toList();
+      });
+    } catch (e) {
+      debugPrint('failed to load image labels: $e');
+    } finally {
+      imageLabeler.close();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 192, 67),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Image Labeling App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // Display the selected image
+            if (_pickedImage != null)
+              Image.file(
+                _pickedImage!,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              )
+            else
+              Image.asset('assets/no_image.jpg'),
+            const SizedBox(height: 16),
+            // Buttons to pick an image from the camera or gallery
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () => _selectImage(ImageSource.camera),
+                  icon: const Icon(Icons.camera),
+                  label: const Text('Camera'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () => _selectImage(ImageSource.gallery),
+                  icon: const Icon(Icons.photo),
+                  label: const Text('Gallery'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 16),
+            // Display the detected labels
+            Expanded(
+              child: _labels.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _labels.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const Icon(Icons.format_list_bulleted),
+                          title: Text(_labels[index]),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text('No labels Found'),
+                    ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
